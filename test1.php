631f9b500a9e913bda2d04a3cc072ec9cdfda740
michaeltran33 <126439994+michaeltran33@users.noreply.github.com>
2023-03-06 11:47:30 +1000

    Create test1.php

---
+++ test1.php
@@ -0,0 +1,10 @@
+class test1
+{
+    private static $FOO = 2; // Unused
+    private $i = 5; // Unused
+    private $j = 6;
+    public function addOne()
+    {
+        return $this->j++;
+    }
+}
