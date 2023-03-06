25c54a3def3fe42f1d198c3a8f7044d8bffb0f68
michaeltran33 <126439994+michaeltran33@users.noreply.github.com>
2023-03-06 11:39:57 +1000

    Update main.php

--- main.php
+++ main.php
@@ -1,10 +1,11 @@
 $email = "example@example.com"; // replace with the email address you want to validate
-
+  $i = 5; // Unused
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "Valid email";
 } else {
     echo "Invalid email";
     echo "Invalid email";
+
     echo "this is for testing, one more line. It was problem of push";
 }
 
