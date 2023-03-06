b48808ecc4218b892391de89e5463c7ffbaa6e6a
michaeltran33 <126439994+michaeltran33@users.noreply.github.com>
2023-03-06 13:48:29 +1000

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,11 +1,11 @@
 class test1
 {
-    private static $FOO = 2; // Unused
+    private static $FOO = 2; // Unused - expected sonarcloud find it
     private $i = 5; // Unused
     private $j = 6;
     public function addOne()
     {
         return $this->j++;
-        return $this->j++; // expected fail for dup n after retun
+        return $this->j++; // expected fail for dup n after return
     }
 }
